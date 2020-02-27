99f0eec48e6ffd494437c5890c6e35903ce9ff4f
vinodsunkara <37222937+vinodsunkara@users.noreply.github.com>
2020-02-27 14:40:55 -0500

    Add README

--- provider.tf
+++ provider.tf
@@ -2,9 +2,9 @@
 
 provider "azurerm" {
   
-  subscription_id = "1b85196d-f957-4779-8d3f-4a335a64c4a7"
-  tenant_id = "b7799b39-1302-4ad7-b54b-c6036c12fd2c"
-  client_id = "a8451363-c0f8-4df6-9538-bc66647ca6c1"
-  client_secret = "W?E=/GXA-i9qm5vOGnCiAC5cNGhdAek6"
+  subscription_id = "**********"
+  tenant_id = "**************"
+  client_id = "*************"
+  client_secret = "************"
   features {}
 }
