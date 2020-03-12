88248142f80a2182f5f825ac7171cb91903ffcfe
vinodsunkara <37222937+vinodsunkara@users.noreply.github.com>
2020-03-11 20:55:06 -0400

    Updated mainvar.tf

--- mainvar.tf
+++ mainvar.tf
@@ -14,9 +14,9 @@ variable "location" {
 
 variable "azure_details" {
   default = {
-      subscription_id = "_subscription_id_"
-      client_id = "_client_id_"
-      tenant_id = "_tenant_id_"
-      client_secret = "_client_secret_"
+      subscription_id = "_subscriptionid_"
+      client_id = "_clientid_"
+      tenant_id = "_tenantid_"
+      client_secret = "_clientsecret_"
   }
 }
