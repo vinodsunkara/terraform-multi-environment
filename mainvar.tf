cbf817f5e31c4abcc5f7ce3f2b4903af144c807c
vinodsunkara <37222937+vinodsunkara@users.noreply.github.com>
2020-03-11 20:14:35 -0400

    Updated the Readme.md file

--- mainvar.tf
+++ mainvar.tf
@@ -14,9 +14,9 @@ variable "location" {
 
 variable "azure_details" {
   default = {
-      subscription_id = "1b85196d-f957-4779-8d3f-4a335a64c4a7"
-      client_id = "a8451363-c0f8-4df6-9538-bc66647ca6c1"
-      tenant_id = "b7799b39-1302-4ad7-b54b-c6036c12fd2c"
-      client_secret = "4N[k@Vty_phczsTpKNllWNL3G=VvWW88"
+      subscription_id = "_subscription_id_"
+      client_id = "_client_id_"
+      tenant_id = "_tenant_id_"
+      client_secret = "_client_secret_"
   }
 }
